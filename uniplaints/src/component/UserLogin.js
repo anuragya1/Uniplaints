b56517f1005e5f204870bef5ed58b0c11fa74b47
anuragya1 <151569428+anuragya1@users.noreply.github.com>
2024-08-06 15:48:42 +0530

    Update UserLogin.js
    just adjusted some minor mistakes

--- uniplaints/src/component/UserLogin.js
+++ uniplaints/src/component/UserLogin.js
@@ -22,7 +22,7 @@ export default function UserLogin() {
     navigate('/Exepanel');
     else{
       try {
-        const response = await fetch('http://localhost:3001/login', {
+        const response = await fetch('https://backend-mocha-kappa-93.vercel.app/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
