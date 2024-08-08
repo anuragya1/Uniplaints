7ebe187d4ce83c0c5f2ff722c5e3de7c28065187
lenovo pc <you@example.com>
2024-08-08 10:26:13 +0530

    backend routes working

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ const port = 3001;
 app.use(cors({
   origin: 'https://uniplaints-y17b.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 }));
 app.use(express.json());
 app.use('/',router);
