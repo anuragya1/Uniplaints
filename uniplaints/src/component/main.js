c84d5874512b3b3ea9f94fb2f383f7a72b7cee5f
anuragya1 <151569428+anuragya1@users.noreply.github.com>
2024-08-08 10:11:45 +0530

    Update main.js

--- uniplaints/src/component/main.js
+++ uniplaints/src/component/main.js
@@ -12,7 +12,7 @@ const LandingPage = () => {
   const [TotalcompResolved, setTotalcompResolved] = useState(null);
   const getData = async () => {
     try {
-      let response = await fetch('https://backen-kyumgdki0-anurags-projects-dc4e4a37.vercel.app/Data');
+      let response = await fetch('https://backen-c9mc6rw6y-anurags-projects-dc4e4a37.vercel.app/Data');
       if (response.ok) {
         const complaint = await response.json();
         setTotalComplaints(complaint.total);
