b772e43c476e44eba845ddaf4db8013141189877
anuragya1 <151569428+anuragya1@users.noreply.github.com>
2024-08-08 10:05:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ import cors from 'cors';
 import router from './Routes.js';
 const app = express();
 const port = 3001;
-app.use(cors());
+app.use(cors({
+  origin: 'https://uniplaints-y17b.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use('/',router);
 app.listen(port, () => {
