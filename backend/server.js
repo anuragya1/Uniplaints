9dcacfb545a9c330fde7cf3b72afe9aa6f713acd
lenovo pc <you@example.com>
2024-08-10 13:38:26 +0530

    another change

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import router from './Routes.js';
+
 const app = express();
 const port = 3001;
-app.use(cors({
-  origin: 'https://uniplaints-y17b.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-app.use(express.json());
-app.use('/',router);
-app.listen(port, () => {
+
+app.use(cors());
+app.use('/', router);
+
+app.listen(port, () => { 
   console.log(`Server is running on port ${port}`);
 });
-export default  app;
+
+export default app;
